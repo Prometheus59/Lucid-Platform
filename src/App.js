d4fb05f12178aa654f9d6c835930af0a1bab0bb2
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-12-30 19:19:20 -0500

    Added a main page, smooth hashlink scrolling, main page divs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import logo from './moon.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./moon.svg";
+import "./App.css";
+import { HashLink as Link } from "react-router-hash-link";
+import { HashRouter } from "react-router-dom";
+import Mainpage from "./Mainpage/Mainpage";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" id="path" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <div className="App">
+          <div className="intro">
+            <img src={logo} className="App-logo" id="path" alt="logo" />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint a
+              corrupti possimus magni ut repellat neque explicabo, fuga autem
+              labore?
+            </p>
+            <Link smooth to="#mainpage" className="App-link">
+              Enter
+            </Link>
+          </div>
+          <Mainpage />
+          <footer />
+        </div>
+      </HashRouter>
     );
   }
 }
---
+++ src/Mainpage/Mainpage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Mainpage.css";
+
+class Mainpage extends React.Component {
+  render() {
+    return (
+      <div id="mainpage">
+        <div>Blog</div>
+        <div>New Entry</div>
+        <div>Previous Entries</div>
+        <div>Extra Resources</div>
+        <div>Community</div>
+      </div>
+    );
+  }
+}
+
+export default Mainpage;
